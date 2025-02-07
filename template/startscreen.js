cfdcb609accb87e9470ddb0aa731619042e014b6
Daniele Dona <d.dona01@gmail.com>
2025-02-07 17:29:05 +0100

    Added startscreen symbols

--- template/startscreen.js
+++ template/startscreen.js
@@ -5,8 +5,12 @@ function loadStartScreen() {
 
   startScreen.innerHTML = /*html*/ `
     <div class='start-container'>
-
-
+      <div class='start-screen-container'>
+        <img src="./img/start_screen/settings.svg" alt="settings">
+        <img src="./img/start_screen/sound_on.svg" alt="sound-on">
+        <img src="./img/start_screen/fullscreen-enter.svg" alt="fullscreen">
+      </div>
+      <img class="play-icon" src="./img/start_screen/play-button.svg" alt="play-button">
     </div>
   `;
 }
