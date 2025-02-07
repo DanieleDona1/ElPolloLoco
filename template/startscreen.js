6679a8ac734a22b9e2a47cf1e9eba341f01707fb
Daniele Dona <d.dona01@gmail.com>
2025-02-07 13:13:48 +0100

    Added startscreen responiv

---
+++ template/startscreen.js
@@ -0,0 +1,12 @@
+
+
+function loadStartScreen() {
+  let startScreen = document.getElementById('userInteraction');
+
+  startScreen.innerHTML = /*html*/ `
+    <div class='start-container'>
+
+
+    </div>
+  `;
+}
