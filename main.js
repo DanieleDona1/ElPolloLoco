9decfc9ede20659749afbb6e0c2cc10caff21029
Daniele Dona <d.dona01@gmail.com>
2025-02-09 11:08:49 +0100

    Added favicon

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 function onload() {
   loadStartScreen();
+  toggleSound();
+}
+
+function playGame() {
+  // isBackgroundSoundOn = true;
+  // toggleSound();
+  let startScreen = document.getElementById('userInteraction');
+  startScreen.innerHTML = `<canvas id="canvas" width="720" height="480"></canvas>`;
+
+  init();
 }
--- template/startscreen.js
+++ template/startscreen.js
@@ -1,16 +1,33 @@
+let isBackgroundSoundOn = false;
+let gameSound = new Audio('audio/game_sound.mp3'); // Lade die Audiodatei
+gameSound.loop = true; // Der Sound wird wiederholt abgespielt
 
-
+// Funktion zum Startbildschirm laden
 function loadStartScreen() {
   let startScreen = document.getElementById('userInteraction');
-
   startScreen.innerHTML = /*html*/ `
     <div class='start-container'>
       <div class='start-screen-container'>
         <img src="./img/start_screen/settings.svg" alt="settings">
-        <img src="./img/start_screen/sound_on.svg" alt="sound-on">
+        <img id="sound-icon" src="./img/start_screen/sound_off.svg" alt="sound-on" onclick="toggleSound()">
         <img src="./img/start_screen/fullscreen-enter.svg" alt="fullscreen">
       </div>
-      <img class="play-icon" src="./img/start_screen/play-button.svg" alt="play-button">
+      <img onclick="playGame();" class="play-icon" src="./img/start_screen/play-btn.svg" alt="play-button">
     </div>
   `;
+}
+
+// Funktion, um den Sound ein- oder auszuschalten
+function toggleSound() {
+  let soundIcon = document.getElementById('sound-icon');
+  if (isBackgroundSoundOn) {
+    gameSound.play();
+    soundIcon.src = "./img/start_screen/sound_on.svg";
+  } else {
+    // Stoppe den Sound
+    gameSound.pause();
+    gameSound.currentTime = 0;
+    soundIcon.src = "./img/start_screen/sound_off.svg";
+  }
+  isBackgroundSoundOn = !isBackgroundSoundOn;
 }
