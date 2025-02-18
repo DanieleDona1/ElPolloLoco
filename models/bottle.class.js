1d8c3d42a86eaa520ab55f37a845f8ffbb6d0568
Daniele Dona <d.dona01@gmail.com>
2025-02-18 15:52:56 +0100

    Small changes legal info

--- models/bottle.class.js
+++ models/bottle.class.js
@@ -2,12 +2,13 @@ class Bottle extends MovableObject {
   y = 345;
   height = 80;
   width = 80;
-  
+
 
   constructor() {
     super()
     this.loadImage('./img/6_salsa_bottle/1_salsa_bottle_on_ground.png');
     this.x = 300 + Math.random() * 2000;
+    // this.y = 10 + Math.random()* 700;
     this.animate();
 
   }
--- models/coin.class.js
+++ models/coin.class.js
@@ -7,7 +7,7 @@ class Coin extends MovableObject {
     super().loadImage('./img/8_coin/coin_1.png');
     // this.x = Math.random() * 500;
     this.x = 300 + Math.random() * 2100;
-    this.y = 30 + Math.random()* 200;
+    this.y = 30 + Math.random()* 170;
     // this.animate();
 
   }
